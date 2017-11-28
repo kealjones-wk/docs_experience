18923a4863b2266b79bb4f0738b878d9c6e7c1ca
Joel Leibow <joel.leibow@workiva.com>
2017-11-28 10:38:50 -0700

    Markup tweaks and dispatch from event target rather than document

--- lib/src/docs_experience_app.dart
+++ lib/src/docs_experience_app.dart
@@ -1,4 +1,7 @@
+import 'dart:html';
+
 import 'package:over_react/over_react.dart';
+import 'package:shell_events/shell_events.dart' show ShellToggleMessagesEvent;
 
 @Factory()
 UiFactory<DocsExperienceAppProps> DocsExperienceApp;
@@ -14,21 +17,33 @@ class DocsExperienceAppState extends UiState {
 
 @Component()
 class DocsExperienceAppComponent extends UiStatefulComponent<DocsExperienceAppProps, DocsExperienceAppState>  {
+  ReactElement _toggleMessagesButton;
+
   Map getInitialState() => (newState()
     ..counter = 0
   );
   
   render() {
     return Dom.div()(
-      Dom.h3()('Docs: using over_react 1.17.0'),
-      Dom.span()('Counter: ' + state.counter.toString()),
+      (Dom.h4()..style = { 'margin': 0 })('Docs: using over_react 1.17.0'),
+      Dom.p()('Counter: ' + state.counter.toString()),
       Dom.div()(
-        (Dom.button()
-          ..onClick = (event) => setState(newState()..counter = ++state.counter)
+        Dom.p()('Docs Operations'),
+        (Dom.button()..onClick = (event) => setState(newState()..counter = ++state.counter)
         )('Increment'),
-        (Dom.button()
-          ..onClick = (event) => setState(newState()..counter = --state.counter)
+        (Dom.button()..onClick = (event) => setState(newState()..counter = --state.counter)
         )('Decrement')
+      ),
+      Dom.div()(
+        Dom.p()('Shell Operations'),
+        (Dom.button()
+          ..onClick = (event) {
+            findDomNode(_toggleMessagesButton).dispatchEvent(new ShellToggleMessagesEvent());
+          }
+          ..ref = (ref) {
+            _toggleMessagesButton = ref;
+          }
+        )('Toggle Shell Messages')
       )
     );
   }
